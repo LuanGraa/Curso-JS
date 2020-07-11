1149bc12adac95513439bf3938dae9a6d844d0f0
LuanGraa <49499491+LuanGraa@users.noreply.github.com>
2020-07-11 19:26:12 +0100

    Lesson-3 Comments

---
+++ Lesson-3/comments.js
@@ -0,0 +1,14 @@
+//Test comment
+console.log("Text1");
+console.log("Text2");
+//comment 
+console.log("Text3");
+/* 
+multi line 
+commenting
+*/
+console.log("Text4");
+//Another test comment
+/* Code inside comments will be ignored
+console.log("Text6");
+*/
