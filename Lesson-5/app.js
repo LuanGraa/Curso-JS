e44e25fab774a1b31b9a22c936cad4171dba162f
LuanGraa <49499491+LuanGraa@users.noreply.github.com>
2020-07-12 20:05:32 +0100

    Lesson 5 Variables

---
+++ Lesson-5/app.js
@@ -0,0 +1,22 @@
+let name = "John";
+let variable;
+console.log(name,"is a name");
+console.log(variable); //unitialized variables are undefined
+variable = 1;//defined as int
+console.log(variable);
+
+//let variable; //will cause variable redefinition error
+
+//let console; //illegal variable name 
+
+//let 1variable; //variables cannot start with a number
+
+//let va-riable; //variable cannot posess illegal characters
+
+//Variables should use camelCase formatting
+let variableOne = 1;
+
+//Variables are case sensitive
+let variableone = 2;
+
+console.log(variableOne,variableone);
