fa5d29fbe1db5f9f2784beb0dfe991f042e566e4
LuanGraa <49499491+LuanGraa@users.noreply.github.com>
2020-07-12 20:37:16 +0100

    Lesson 6 Constants

---
+++ Lesson-6/constants.js
@@ -0,0 +1,12 @@
+//Constants follow the same naming rules as normal variables
+
+const name = "John doe";
+
+//constants cannot be redefined
+//name = "Jane doe"; 
+
+console.log(name);
+
+//constants have to be initialized
+//const constant; 
+
